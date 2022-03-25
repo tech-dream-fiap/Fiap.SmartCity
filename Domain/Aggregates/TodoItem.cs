931eed01ca3590111e55ef48e892173fccdfebf2
root-marco <rootslowed@gmail.com>
2022-03-24 23:11:49 -0300

    initial commit

---
+++ Domain/Aggregates/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Aggregates
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Aggregates;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
